6d57c4db74a85a01b61b13b6930b1be5cbc47d15
Andy Sheehan <andrew.sheehan@hiya.com>
2025-03-12 12:19:56 -0700

    Fixed html to - replacement

--- android.js
+++ android.js
@@ -26,7 +26,7 @@ class AndroidFunctions extends DocFunctions {
             }
         }
         const newString = mainContent.innerHTML
-        const withoutEnding = path.basename(file).replace("-.html", "-")
+        const withoutEnding = path.basename(file).replace(".html", "-")
         const newHtmlPath = path.join(this.dest, this.folder, path.relative(this.src, path.dirname(file)), withoutEnding + ".source")
         const newMdxPath = path.join(this.dest, this.folder, path.relative(this.src, path.dirname(file)), withoutEnding + ".mdx")
         fs.outputFileSync(newHtmlPath, newString)
